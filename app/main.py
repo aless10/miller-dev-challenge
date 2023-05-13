5ef94d5cc6f8632e96e5176831532c055afbbcb3
Alessio Izzo <alessio.izzo86@gmail.com>
2023-05-13 17:50:35 +0200

    add init db on startup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
 from .api import api_router
+from .db.session import db_engine
 
 app = FastAPI(title='Miller Challenge')
 
-# asyncio.get_event_loop().create_task(db_engine.create_all())
+
+@app.on_event('startup')
+async def init_db() -> None:
+    await db_engine.create_all()
+
 
 app.include_router(api_router)
